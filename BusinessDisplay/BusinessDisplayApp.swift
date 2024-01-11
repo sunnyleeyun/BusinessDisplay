0f658dc245787bb170da52f47010f6520b23790a
sunnyleeyun <sunnylee945@hotmail.com>
2024-01-11 17:59:41 -0500

    Initial Commit

---
+++ BusinessDisplay/BusinessDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessDisplayApp.swift
+//  BusinessDisplay
+//
+//  Created by 李昀 on 2024/1/11.
+//
+
+import SwiftUI
+
+@main
+struct BusinessDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusinessDisplay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BusinessDisplay
+//
+//  Created by 李昀 on 2024/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
