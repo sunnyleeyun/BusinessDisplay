eb839c9b10b9dc9a2e1ae502b261d6bc4f64e63a
sunnyleeyun <sunnylee945@hotmail.com>
2024-01-12 12:44:37 -0500

    feat: add business service actual fetching

--- BusinessDisplay/Services/BusinessService.swift
+++ BusinessDisplay/Services/BusinessService.swift
@@ -10,3 +10,29 @@ import Foundation
 protocol BusinessFetching {
     func fetchLocation() async throws -> Business
 }
+
+class BusinessService: BusinessFetching {
+    private var networkManager: NetworkManager
+    private let baseUrl = "https://purs-demo-bucket-test.s3.us-west-2.amazonaws.com"
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchLocation() async throws -> Business {
+        guard let url = URL(string: "\(baseUrl)/location.json") else {
+            fatalError("Invalid Url")
+        }
+        return try await withCheckedThrowingContinuation { continuation in
+            networkManager.request(fromUrl: url, httpMethod: .get) { (result: Result<Business, Error>) in
+                switch result {
+                case .success(let response):
+                    continuation.resume(returning: response)
+                case .failure(let error):
+                    debugPrint(error.localizedDescription)
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+}
