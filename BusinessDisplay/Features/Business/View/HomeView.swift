ea2e40907f90dc55e61b2499fcbb2fcd51a00991
sunnyleeyun <sunnylee945@hotmail.com>
2024-01-12 12:39:59 -0500

    feat: add basic home and background image

---
+++ BusinessDisplay/Features/Business/View/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  BusinessDisplay
+//
+//  Created by 李昀 on 2024/1/12.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        GeometryReader { geo in
+            
+            ZStack {
+                Image("Wallpaper")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                    .opacity(1)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
