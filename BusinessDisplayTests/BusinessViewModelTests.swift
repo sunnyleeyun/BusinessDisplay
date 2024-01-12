299df9dab7b9fa2dae3129ad3bfdb25253a54f59
sunnyleeyun <sunnylee945@hotmail.com>
2024-01-12 02:20:29 -0500

    feat: add hours handling and tests

--- BusinessDisplayTests/BusinessViewModelTests.swift
+++ BusinessDisplayTests/BusinessViewModelTests.swift
@@ -17,21 +17,6 @@ final class BusinessViewModelTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
     func testBusinessLoaded() async {
         let mock = MockBusinessService()
         let businessVM = BusinessViewModel(businessFetching: mock)
@@ -41,9 +26,58 @@ final class BusinessViewModelTests: XCTestCase {
         XCTAssertEqual(businessVM.business!.hours.count, 9)
     }
     
-    func testOpeningHours() {
+    func testOpeningHours() async {
         let mock = MockBusinessService()
         let businessVM = BusinessViewModel(businessFetching: mock)
-        XCTAssertEqual(businessVM.business?.openingHours().count, 7)
+        await businessVM.getBusiness();
+        let openingHours = businessVM.business!.openingHours()
+        XCTAssertEqual(openingHours.count, 7)
+        XCTAssertEqual(openingHours[0].1.count, 0)
+        XCTAssertEqual(openingHours[1].1.count, 2)
+        XCTAssertEqual(openingHours[2].1.count, 2)
+        XCTAssertEqual(openingHours[3].1.count, 1)
+        XCTAssertEqual(openingHours[4].1.count, 1)
+        XCTAssertEqual(openingHours[5].1.count, 1)
+        XCTAssertEqual(openingHours[6].1.count, 2)
+    }
+    
+    func testOpenStatus1() async {
+        let mock = MockBusinessService()
+        let businessVM = BusinessViewModel(businessFetching: mock)
+        await businessVM.getBusiness();
+        let openStatus = businessVM.business!.openStatus(currentTimestamp: 1705031145, periodCase: .LOWER) // 2024.01.11 22:45:45 (THU)
+        XCTAssertEqual(openStatus, "Open until 12am")
+    }
+    
+    func testOpenStatus2() async {
+        let mock = MockBusinessService()
+        let businessVM = BusinessViewModel(businessFetching: mock)
+        await businessVM.getBusiness();
+        let openStatus = businessVM.business!.openStatus(currentTimestamp: 1705034700, periodCase: .LOWER) // 2024.01.11 23:45:00 (THU)
+        XCTAssertEqual(openStatus, "Open until 12am, reopens at 7am")
+    }
+    
+    func testOpenStatus3() async {
+        let mock = MockBusinessService()
+        let businessVM = BusinessViewModel(businessFetching: mock)
+        await businessVM.getBusiness();
+        let openStatus = businessVM.business!.openStatus(currentTimestamp: 1705037410, periodCase: .LOWER) // 2024.01.12 00:30:10 (FRI)
+        XCTAssertEqual(openStatus, "Opens again at 7am")
+    }
+    
+    func testOpenStatus4() async {
+        let mock = MockBusinessService()
+        let businessVM = BusinessViewModel(businessFetching: mock)
+        await businessVM.getBusiness();
+        let openStatus = businessVM.business!.openStatus(currentTimestamp: 1705293010, periodCase: .LOWER) // 2024.01.14 23:30:10 (SUN)
+        XCTAssertEqual(openStatus, "Opens Tuesday 7am")
+    }
+    
+    func testOpenStatus5() async {
+        let mock = MockBusinessService()
+        let businessVM = BusinessViewModel(businessFetching: mock)
+        await businessVM.getBusiness();
+        let openStatus = businessVM.business!.openStatus(currentTimestamp: 1705433410, periodCase: .LOWER) // 2024.01.16 14:30:10 (TUE)
+        XCTAssertEqual(openStatus, "Opens again at 3pm")
     }
 }
